377b22dd7f7d5b98767c697922a90f366dc152a1
muhammed mubarak <oyindamola850@gmail.com>
2022-11-28 21:08:54 +0100

    update

--- src/pages/forgotPassword/ForgotPassword.tsx
+++ src/pages/forgotPassword/ForgotPassword.tsx
@@ -34,7 +34,7 @@ const ForgotPassword = () => {
             <ButtonWrapper>
               <CustomButton>Submit</CustomButton>
               <LinkText>
-                <AuthLink to="/login">Go Back to login</AuthLink>
+                <AuthLink to="/">Go Back to login</AuthLink>
               </LinkText>
             </ButtonWrapper>
           </ForgotWrapper>
